cc0efc179421ccb1ad9a41849be0733a1db75074
Ravi Teja <tejamaramreddy@gmail.com>
2021-04-12 10:38:30 +0530

    postman-request changed to request

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,4 +1,4 @@
-const request = require('postman-request')
+const request = require('request')
 const geocode = (address,callback)=>{
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(address)+'.json?access_token=pk.eyJ1IjoidGVqYW1hcmFtcmVkZHkiLCJhIjoiY2tuNjV2bXhjMGF6NDJ1czZhNnprcDRyMyJ9.WmdcodslvJcTgsTvGgZB2w';
     request({url},(error,response)=>{
